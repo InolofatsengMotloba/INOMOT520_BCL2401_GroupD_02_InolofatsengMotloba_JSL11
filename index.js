4afcb70a90942730fe5d6a976b5375c63753807e
Inolofatseng Motloba <motlobainolo123@gmail.com>
2024-04-22 20:03:11 +0200

    Import helper functions from utils

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 // TASK: import helper functions from utils
+import { getTasks } from "./utils/taskFunctions.js";
+import { createNewTask } from "./utils/taskFunctions.js";
+import { patchTask } from "./utils/taskFunctions.js";
+import { putTask } from "./utils/taskFunctions.js";
+import { deleteTask } from "./utils/taskFunctions.js";
 // TASK: import initialData
 
 
